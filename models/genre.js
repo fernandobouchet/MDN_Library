c22babe1ce7d56e85cea67a1b46ddc13d5fe7d46
fernandobouchet <fernandobouchet@gmail.com>
2022-09-21 23:33:12 -0300

    Fix missing brackets

--- models/genre.js
+++ models/genre.js
@@ -2,10 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
-  name: String,
-  required: true,
-  minLength: 3,
-  maxLength: 100,
+  name: { String, required: true, minLength: 3, maxLength: 100 },
 });
 
 GenreSchema.virtual('url').get(function () {
