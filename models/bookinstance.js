6f7a9b950fb2227eaa6e769b9cf49caf19cbc5cf
fernandobouchet <fernandobouchet@gmail.com>
2022-10-01 13:08:07 -0300

    Add 'bookinstance_detail' view and controller

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -19,5 +19,10 @@ BookInstanceSchema.virtual('due_back_formatted').get(function () {
   return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
+BookInstanceSchema.virtual('url').get(function () {
+  // We don't use an arrow function as we'll need the this object
+  return '/catalog/bookinstance/' + this._id;
+});
+
 // Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
