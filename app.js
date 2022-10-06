ce216410ff91fa441625642b786627a774d044a3
fernandobouchet <fernandobouchet@gmail.com>
2022-10-05 22:44:05 -0300

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -8,12 +8,14 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
 const compression = require('compression');
+const helmet = require('helmet');
 
 var app = express();
 
 const mongoose = require('mongoose');
-const mongoDB =
+const dev_db_url =
   'mongodb+srv://admin_library:fhsgWIF15zaZP8wY@cluster0.khsupcr.mongodb.net/?retryWrites=true&w=majority';
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -25,6 +27,7 @@ db.on('error', console.error.bind(console, 'MongoDB conecction error:'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(helmet());
 app.use(compression());
 app.use(logger('dev'));
 app.use(express.json());
